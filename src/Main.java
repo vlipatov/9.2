1a43280df635a90a119754be6a436543f597317f
vlipatov <vadegg0607@gmail.com>
2020-01-28 12:01:59 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.Scanner;
@@ -8,26 +7,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите путь к папке. \nФормат для ввода - C:\\Users\\username\\folderName");
-        String path = scanner.nextLine();
+        String source = scanner.nextLine();
         System.out.println("Введите путь назначения");
-        String newPath = scanner.nextLine();
-        folderCopy(path, newPath);
-
+        String dest = scanner.nextLine();
+        folderCopy(source, dest);
     }
 
-    private static void folderCopy(String path, String newPath) throws IOException {
-
-        Path source = Paths.get(path);
-        Path destination = Paths.get(newPath);
-//        Files.createDirectory(destination);
-        File folder = new File(path);
-        File[] files = folder.listFiles();
-
-        try {
-            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("Папка скопирована!");
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static void folderCopy(String source, String destination) throws IOException {
+        File sourceDir = new File(source);
+        File[] listOfFiles = sourceDir.listFiles();
+        Path destDir = Paths.get(destination);
+        if(!destDir.toFile().isDirectory()) { // если по пути назначения нет папки, то создаем новую
+            Files.createDirectory(destDir);
         }
+        if (listOfFiles != null)
+            for (File file : listOfFiles)
+                Files.copy(file.toPath(), destDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
     }
 }
